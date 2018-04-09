87dd93eac0f53006a1dd2d853928c815cf6fcc87
Andrey Bar <mazafakand@gmail.com>
2018-04-09 19:12:37 +0300

    added routing, checkout and orders cpmponents

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,28 +88,37 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false });
   }
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Andrey Barchuk',
-        adress: {
-          street: 'TestStreet 1',
-          zipCode: '23123',
-          country: 'Ukraine'
-        },
-        email: 'rest@test.com'
-      },
-      deliveryMethod: 'fastest'
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Andrey Barchuk',
+    //     adress: {
+    //       street: 'TestStreet 1',
+    //       zipCode: '23123',
+    //       country: 'Ukraine'
+    //     },
+    //     email: 'rest@test.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // axios.post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch(error => {
+    //     this.setState({ loading: false, purchasing: false })
+    //   });
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false })
-      });
+    const queryString = queryParams.join('&');
+    this.props.history.push({
+      pathname: './checkout',
+      search: '?' + queryString
+    });
   }
 
   render() {
